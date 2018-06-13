0f8a54e5464e3cc91b64a3bb928f335b94fdad1b
patriciafig <patricia.figueroa@ucdenver.edu>
2018-06-13 12:10:27 -0600

    initial commit

--- wiki-vote.py
+++ wiki-vote.py
@@ -2,15 +2,45 @@
 # Patricia Figueroa
 # CSCI 4800 - Programming Assignment 1
 
-
 import networkx as nx
-
+from sets import *
 
 g = nx.read_adjlist('wiki-Vote.txt', create_using=nx.DiGraph())
-g_undir = g.to_undirected()
+g_undirected = g.to_undirected()
+
+#degrees = g.degree().values()
+#in_degrees = g.in_degree().values()
+#out_degrees = g.out_degree().values()
+#list(g.in_degree().values())
+
 
 print 'Number nodes in the network:', len(g)
 
 print 'Number of directed edges in the network:', len(g.edges())
 
-print 'Number of undirected edges in the network:', len(g_undir.edges())
+print 'Number of undirected edges in the network:', len(g_undirected.edges())
+
+count = 0
+for node in g.nodes():
+    if g.out_degree(node) > 100:
+        count += 1
+print 'Number of nodes with more than 100 outgoing edges:', count
+
+
+zero_out_nodes = Set()
+for node in g.nodes():
+    if g.out_degree(node) == 0:
+        zero_out_nodes.add(node)
+
+print 'Number of nodes of zero out-degree:', len(zero_out_nodes)
+
+
+zero_in_nodes = Set()
+for node in g.nodes():
+    if g.in_degree(node) == 0:
+        zero_in_nodes.add(node)
+
+
+print 'Number of nodes of zero in-degree:', len(zero_in_nodes)
+
+
